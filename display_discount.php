3bb38175f45dc5e75aec64d1970c833555ad43c7
Lionel Alliaj <la92@afsconnect2.njit.edu>
2017-02-05 19:06:56 -0500

    Changes to display_discount.php file

--- display_discount.php
+++ display_discount.php
@@ -1,36 +1,39 @@
 <?php
-  $description = $_POST['product_description'];
-  $list_price = $_POST['list_price'];
-  $discount_percent = $_POST['discount_percent'];
-  // calculate the discount
+  //get the data from the form
+  $product_description= $_POST['product_description'];
+  $list_price= $_POST['list_price'];
+  $discount_percent= $_POST['discount_percent'];
+  
+  //calculate the discount
   $discount = $list_price * $discount_percent * .01;
   $discount_price = $list_price - $discount;
 			    
-   // apply currency formatting to the dollar andpercent amounts
-  $list_price_formatted ="$".number_format($list_price, 2);
-  $discount_percent_formatted =$discount_percent."%";
-  $discount_formatted ="$".number_format($discount, 2);
-  $discount_price_formatted ="$".number_format($discount_price,2);            
+  //apply currency formatting to the dollar andpercent amounts
+  $list_price_formatted = "$".number_format($list_price, 2);
+  $discount_percent_formatted = number_format($discount_percent,1)."%";
+  $discount_formatted = "$".number_format($discount, 2);
+  $discount_price_formatted = "$".number_format($discount_price,2);            
 						    
-  // escape theunformatted input
-  $product_description_escaped=htmlspecialchars($product_description);
+  // escape the unformatted input
+  $product_description_escaped = htmlspecialchars($product_description);
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Product Discount Calculator</title>
-	<link rel="stylesheet" type="text/css" href="main.css">
+	<link rel="stylesheet" type="text/css" href="main.css"
 </head>
 <body>
 	<main>
-		<h1> Product Discount Calculator</h1>
+		<h1>Producut Discount Calculator</h1>
+
 		<label>Product Description:</label>
-		<span><?php echo $product_description_escaped;?></span><br>
+		<span><?php echo $product_description_escaped; ?></span><br>
 
 		<label>List Price:</label>
 		<span><?php echo $list_price_formatted; ?></span><br>
-
+		
 		<label>Standard Discount:</label>
 		<span><?php echo $discount_percent_formatted; ?></span><br>
 
@@ -38,10 +41,7 @@
 		<span><?php echo $discount_formatted; ?></span><br>
 
 		<label>Discount Price:</label>
-		<span><?php echo $discount_price_formatted; ?></span><br>
+		<span><?php echo $discount_price_formatter; ?></span><br>
 	</main>
 </body>
 </html>
-
-
-
